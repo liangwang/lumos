7b423f1e4bed1f9b89d2d173f7d7af61722da2bd
Liang Wang <liang.wang@virginia.edu>
2011-05-04 13:50:57 -0400

    @plot: add one more plot item

--- python/plot.py
+++ python/plot.py
@@ -138,6 +138,101 @@ class SpeedupToUtilizationPlot(Plot):
     def setTechnodeList(self, tlist):
         self._tech_nodes = tlist
 
+class UtilizationToBudgetPlot(Plot):
+    def __init__(self, name='itrs'):
+        Plot.__init__(self, name)
+
+
+        self._tech_nodes = [45, 32, 22, 16, 11, 8]
+        self._f_ratio = [1,0.99,0.9,0.8,0.5,0.01]
+
+        self._mech = 'itrs'
+
+
+    def __writeScript(self):
+        sfname = '%s.gpi' % self._name
+        slines = []
+
+        self._opt_list['figsize'] = '8.5in,22in'
+        figtitle = 'Utilization ratio plotted to budget of area and power'
+        slines.append('##FIGSIZE=%s\n' % self._opt_list['figsize'])
+        slines.append("set multiplot layout 6,2 title '%s'\n" % figtitle)
+        slines.append('set xrange [100:540]\n')
+        slines.append('set xlabel "Area(mm^2)"\n')
+        slines.append('set yrange [100:200]\n')
+        slines.append('set ylabel "Power(W)"\n')
+        slines.append('set zrange [0:1.1]\n')
+        slines.append('set cbrange [0:1]\n')
+        slines.append('set xyplane 0\n')
+        slines.append('set contour\n')
+        slines.append('set cntrparam levels incremental 0.2,0.2,1\n')
+        slines.append('set grid xtics ytics\n')
+
+        for tech in self._tech_nodes:
+            plottitle = 'IO-%dnm' % tech
+            slines.append("set title '%s'\n" % plottitle)
+            slines.append("splot '%s-io-%dnm.dat' with pm3d notitle\n" % (self._name, tech))
+
+            plottitle = 'O3-%dnm' % tech
+            slines.append("set title '%s'\n" % plottitle)
+            slines.append("splot '%s-o3-%dnm.dat' with pm3d notitle\n" % (self._name, tech))
+
+        with open(sfname, 'wb') as f:
+            f.writelines(slines)
+
+    def __writeData(self):
+        sys = SymmetricSystem()
+
+        for t in self._tech_nodes:
+            dfname = '%s-io-%dnm.dat' % (self._name, t)
+            dlines = []
+            #dlines.append("Area Power Utilizaion\n")
+            sys.set_core(IOCore(tech=t,mech=self._mech))
+            for area in range(100,500,4):
+                for power in range(100,200):
+                    sys.set_budget({'area':area,'power':power})
+                    dlines.append("%d %d %g %g\n" % (area, power, sys.urmax, sys.urmax))
+                dlines.append("\n")
+
+            with open(dfname, 'wb') as f:
+                f.writelines(dlines)
+
+            dfname = '%s-o3-%dnm.dat' % (self._name, t)
+            dlines = []
+            #dlines.append("Area Power Utilizaion\n")
+            sys.set_core(O3Core(tech=t,mech=self._mech))
+            for area in range(100,500,4):
+                for power in range(100,200):
+                    sys.set_budget({'area':area,'power':power})
+                    dlines.append("%d %d %g %g\n" % (area, power, sys.urmax, sys.urmax))
+                dlines.append("\n")
+
+            with open(dfname, 'wb') as f:
+                f.writelines(dlines)
+
+
+    def writeFiles(self):
+        self.__writeScript()
+        self.__writeData()
+
+    #def setFigSize(self, size):
+        #self._opt_list['figsize'] = size
+
+    #def setSamples(self, sample):
+        #self._samples = sample
+
+    #def setScalingMechanism(self, mech):
+        #self._mech = mech
+
+    #def setBudget(self, budget):
+        #self._area = budget['area']
+        #self._power = budget['power']
+
+    #def setFRatioList(self, flist):
+        #self._f_ratio = flist
+
+    #def setTechnodeList(self, tlist):
+        #self._tech_nodes = tlist
 
 def plot_to_uratio(sys, applist):
     samples = 1000
@@ -164,42 +259,42 @@ def plot_to_uratio(sys, applist):
 
 if __name__ == '__main__' :
     #techbase.dvfs_aggressive = False
-    applist = [Application(f=1),
-               Application(f=0.99),
-               Application(f=0.9),
-               Application(f=0.8),
-               Application(f=0.5),
-               Application(f=0.01)]
+    #applist = [Application(f=1),
+               #Application(f=0.99),
+               #Application(f=0.9),
+               #Application(f=0.8),
+               #Application(f=0.5),
+               #Application(f=0.01)]
 
-    plt.xlabel('Utilization Ratio')
-    plt.ylabel('Speedup')
-    sys = SymmetricSystem(budget={'area':111,'power':125}, core=O3Core(tech=45))
-    plt.subplot(3,2,1)
-    plot_to_uratio(sys, applist)
+    #plt.xlabel('Utilization Ratio')
+    #plt.ylabel('Speedup')
+    #sys = SymmetricSystem(budget={'area':111,'power':125}, core=O3Core(tech=45))
+    #plt.subplot(3,2,1)
+    #plot_to_uratio(sys, applist)
 
-    sys = SymmetricSystem(core=O3Core(tech=32))
-    plt.subplot(3,2,2)
-    plot_to_uratio(sys, applist)
+    #sys = SymmetricSystem(core=O3Core(tech=32))
+    #plt.subplot(3,2,2)
+    #plot_to_uratio(sys, applist)
 
-    sys = SymmetricSystem(core=O3Core(tech=22))
-    plt.subplot(3,2,3)
-    plot_to_uratio(sys, applist)
+    #sys = SymmetricSystem(core=O3Core(tech=22))
+    #plt.subplot(3,2,3)
+    #plot_to_uratio(sys, applist)
 
-    sys = SymmetricSystem(core=O3Core(tech=16))
-    plt.subplot(3,2,4)
-    plot_to_uratio(sys, applist)
+    #sys = SymmetricSystem(core=O3Core(tech=16))
+    #plt.subplot(3,2,4)
+    #plot_to_uratio(sys, applist)
 
-    sys = SymmetricSystem(core=O3Core(tech=11))
-    plt.subplot(3,2,5)
-    plot_to_uratio(sys, applist)
+    #sys = SymmetricSystem(core=O3Core(tech=11))
+    #plt.subplot(3,2,5)
+    #plot_to_uratio(sys, applist)
 
-    sys = SymmetricSystem(core=O3Core(tech=8))
-    plt.subplot(3,2,6)
-    plot_to_uratio(sys, applist)
+    #sys = SymmetricSystem(core=O3Core(tech=8))
+    #plt.subplot(3,2,6)
+    #plot_to_uratio(sys, applist)
 
     #plt.show()
 
-    p = SpeedupToUtilizationPlot()
+    p = UtilizationToBudgetPlot()
     p.writeFiles()
 
 
