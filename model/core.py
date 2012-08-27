0c45bcb0b865508d336153a2a30a8cceb071c071
Liang Wang <lw2aw@virginia.edu>
2012-08-27 13:12:25 -0400

    Core is modified to an 'abstract' class, use IOCore and O3Core instead

--- model/core.py
+++ model/core.py
@@ -24,18 +24,17 @@ MC_BEST = False
 # SPECfp_rate2006 / 8(cores) * 3 (freq increse by go from 1.6GHz at 65nm to 4.7GHz at 45nm)
 PERF_BASE = 25.69
 
-
-class Core(object):
+class _Core(object):
     """
     Core module
     """
-    def __init__(self, ctype=None, mech=None, tech=None, pv=False, pen_adjust=None, sigma_level=None):
+    def __init__(self, mech=None, tech=None, pv=False, pen_adjust=None, sigma_level=None):
 
         update = True
 
-        if ctype is None:
-            update = False
-        self._ctype = ctype
+        #if ctype is None:
+            #update = False
+        #self._ctype = ctype
 
         if mech is None:
             update = False
@@ -59,115 +58,68 @@ class Core(object):
         if update:
             self.__update_config()
 
+
     def __update_config(self):
         """ Internal function to update value of mech/ctype/tech specific parameters """
         tech = self._tech
         mech = self._mech
-        ctype = self._ctype
         pv = self._pv
 
-        #self._perf0 = math.sqrt(techbase.area[ctype])
         self._perf0 = PERF_BASE
 
-        self._area = techbase.area[ctype] * techscl.area[tech]
+        self._area = self.base_area * techscl.area[tech]
 
         self._fsf = 1
         self._vsf = 1
 
         if mech == 'ITRS' or mech == 'CONS':
-            # Projection-based scaling, 'ITRS' or 'CONS'
-            #self._vt = techbase.vt * techscl.vt[tech]
-            #self._v0 = techbase.vdd * techscl.vdd[mech][tech]
-            #self._f0 = techbase.freq[ctype] * techscl.freq[mech][tech]
-
-            #self._dp0 = techbase.dp[ctype] * techscl.power[mech][tech]
-            #self._sp0 = techbase.sp[ctype] * techscl.power[mech][tech]
-            #if pv:
-                #print 'Projection-based scaling does not support process variation'
             self._vt = projscl.vt[mech][tech]
             v0 = projscl.vdd[mech][tech]
-            f0 = techbase.freq[ctype] * projscl.freq[mech][tech]
+            f0 = self.base_freq * projscl.freq[mech][tech]
             self._perf0 = PERF_BASE * projscl.freq[mech][tech]
             self._bw0 = projscl.freq[mech][tech]
-            #if pv:
-                #mcmodel=freq.PTMScaleMC(MC_CKT, mech, tech, v0, f0)
-                #self._v0 = v0
-                #self._f0 = f0*mcmodel.freq_down
-                #self._f0_nom = f0
-                #if DEBUG:
-                    #print 'mech: %s, tech: %d, freq_down: %g' % (mech, tech, model.freq_down)
-            #else:
             self._v0 = v0
             self._f0 = f0
 
-            self._dp0 = techbase.dp[ctype] * projscl.dp[mech][tech]
-            self._sp0 = techbase.sp[ctype] * projscl.sp[mech][tech]
+            self._dp0 = self.base_dp * projscl.dp[mech][tech]
+            self._sp0 = self.base_sp * projscl.sp[mech][tech]
 
         elif mech == 'HKMGS':
-
             self._vt = ptmscl.vt[mech][tech]
             v0 = ptmscl.vdd[mech][tech]
-            f0 = techbase.freq[ctype] * ptmscl.freq[mech][tech]
+            f0 = self.base_freq * ptmscl.freq[mech][tech]
             self._perf0 = PERF_BASE * ptmscl.freq[mech][tech]
             self._bw0 = ptmscl.freq[mech][tech]
-            #if pv:
-                #mcmodel=freq.PTMScaleMC(MC_CKT, mech, tech, v0, f0)
-                #self._v0 = v0
-                #self._f0 = f0*mcmodel.freq_down
-                #self._f0_nom = f0
-                #if DEBUG:
-                    #print 'mech: %s, tech: %d, freq_down: %g' % (mech, tech, model.freq_down)
-            #else:
             self._v0 = v0
             self._f0 = f0
 
-            self._dp0 = techbase.dp[ctype] * ptmscl.dp[mech][tech]
-            self._sp0 = techbase.sp[ctype] * ptmscl.sp[mech][tech]
-            #self._dp0 = techbase.dp[ctype] * ptmscl.dp[mech][tech]
-            #self._sp0 = techbase.sp[ctype] * ptmscl.sp[mech][tech]
+            self._dp0 = self.base_dp * ptmscl.dp[mech][tech]
+            self._sp0 = self.base_sp * ptmscl.sp[mech][tech]
 
         elif mech == 'LP':
             self._vt = ptmscl.vt[mech][tech]
             v0 = ptmscl.vdd[mech][tech]
-            f0 = techbase.freq[ctype] * ptmscl.freq['HKMGS'][tech] * lpbase.freq[tech]
+            f0 = self.base_freq * ptmscl.freq['HKMGS'][tech] * lpbase.freq[tech]
             self._perf0 = PERF_BASE * ptmscl.freq['HKMGS'][tech] * lpbase.freq[tech]
             self._bw0 = ptmscl.freq['HKMGS'][tech] * lpbase.freq[tech]
-            #if pv:
-                #mcmodel=freq.PTMScaleMC(MC_CKT, mech, tech, v0, f0)
-                #self._v0 = v0
-                #self._f0 = f0*mcmodel.freq_down
-                #self._f0_nom = f0
-                #if DEBUG:
-                    #print 'mech: %s, tech: %d, freq_down: %g' % (mech, tech, model.freq_down)
-            #else:
             self._v0 = v0
             self._f0 = f0
 
             # dynamic power and static power use the same scaling factors
-            #self._dp0 = techbase.dp[ctype] * lpbase.dp[tech] * techscl.power[mech][tech]
-            #self._sp0 = techbase.sp[ctype] * lpbase.sp[tech] * techscl.power[mech][tech]
-            self._dp0 = techbase.dp[ctype] * ptmscl.dp['HKMGS'][tech] * lpbase.dp[tech]
-            self._sp0 = techbase.sp[ctype] * ptmscl.sp['HKMGS'][tech] * lpbase.sp[tech]
+            self._dp0 = self.base_dp * ptmscl.dp['HKMGS'][tech] * lpbase.dp[tech]
+            self._sp0 = self.base_sp * ptmscl.sp['HKMGS'][tech] * lpbase.sp[tech]
 
         if mech == 'ITRS' or mech == 'CONS':
-            #self._vsf_max = 1
-            #self._vsf_min = self._vt/self._v0
 
-            #self._model = freq.ProjectionScale(self._vt, self._v0, self._f0)
-            #self._sp_slope = self._model.sp_slope
             self._vsf_max = 1.1 / self._v0
             self._vsf_min = 0.3 / self._v0
 
             if pv:
                 self._model = freq.PTMScaleMC(MC_CKT, 'HKMGS', tech, v0, f0, self.pen_adjust, self.sigma_level)
                 self.var_penalty = self._model.get_penalty(v0)
-                #self._model_nom = freq.PTMScale(MC_CKT, mech, tech, self._v0, self._f0)
             else:
                 self._model = vfmodel(MC_CKT, 'HKMGS', tech, self._v0, self._f0)
 
-            #if pv:
-                #self._sp_slope = self._mcmodel.sp_slope
-            #else:
             self._sp_slope = self._model.sp_slope
         elif mech == 'HKMGS' or mech == 'LP':
             self._vsf_max = 1.1 / self._v0
@@ -176,14 +128,11 @@ class Core(object):
             if pv:
                 self._model = freq.PTMScaleMC(MC_CKT, mech, tech, v0, f0, self.pen_adjust, self.sigma_level)
                 self.var_penalty = self._model.get_penalty(v0)
-                #self._model_nom = freq.PTMScale(MC_CKT, mech, tech, self._v0, self._f0)
             else:
                 self._model = vfmodel(MC_CKT, mech, tech, self._v0, self._f0)
 
-            #if pv:
-                #self._sp_slope = self._mcmodel.sp_slope
-            #else:
             self._sp_slope = self._model.sp_slope
+
 
     @property
     def tech(self):
@@ -195,10 +144,10 @@ class Core(object):
         """ Get the scaling mechanism, either ITRS or CON(servative) """
         return self._mech
 
-    @property
-    def ctype(self):
-        """ Get the core type, either IO or O3 """
-        return self._ctype
+    #@property
+    #def ctype(self):
+        #""" Get the core type, either IO or O3 """
+        #return self._ctype
 
     @property
     def dp0(self):
@@ -291,7 +240,7 @@ class Core(object):
     ## Update core configuration
     #_config_options = ('mech','ctype','tech','pv')
 
-    def config(self, mech=None, ctype=None, tech=None, pv=None, pen_adjust=None, sigma_level=None):
+    def config(self, mech=None, tech=None, pv=None, pen_adjust=None, sigma_level=None):
 
         update = True
 
@@ -301,11 +250,11 @@ class Core(object):
             update = False
             #logging.debug('mech is not set, failed to config core')
 
-        if ctype is not None:
-            self._ctype = ctype
-        elif self._ctype is None:
-            update = False
-            #logging.debug('ctype is not set, failed to config core')
+        #if ctype is not None:
+            #self._ctype = ctype
+        #elif self._ctype is None:
+            #update = False
+            ##logging.debug('ctype is not set, failed to config core')
 
         if tech is not None:
             self._tech = tech
@@ -341,6 +290,7 @@ class Core(object):
             #kk = '_'+k # translate key into internal name
             #setattr(self,kk,v)
         #self.__update_config()
+
 
     def dvfs_by_factor(self, vsf):
 
@@ -379,10 +329,12 @@ class Core(object):
         else:
             return {'freq': freqs, 'dp': dp, 'sp': sp}
 
-    def set_prop(self, **kwargs):
+    def set_base(self, **kwargs):
         for k, v in kwargs.items():
             k = k.lower()
-            setattr(self, k, v)
+            setattr(self, 'base_'+k, v)
+
+        self.__update_config()
 
     def bandwidth(self, app):
         """Bandwidth consumed by IO core
@@ -392,6 +344,28 @@ class Core(object):
 
         """
         return self._fsf * self._bw0 * app['IO'].bw
+
+
+class IOCore(_Core):
+    def __init__(self, mech=None, tech=None, pv=False, pen_adjust=None, sigma_level=None):
+
+        self.base_area = techbase.area['IO']
+        self.base_freq = techbase.freq['IO']
+        self.base_dp = techbase.dp['IO']
+        self.base_sp = techbase.sp['IO']
+        super(IOCore, self).__init__(mech=mech, tech=tech, pv=pv,
+                pen_adjust=pen_adjust, sigma_level=sigma_level)
+
+
+class O3Core(_Core):
+    def __init__(self, mech=None, tech=None, pv=False, pen_adjust=None, sigma_level=None):
+
+        self.base_area = techbase.area['O3']
+        self.base_freq = techbase.freq['O3']
+        self.base_dp = techbase.dp['O3']
+        self.base_sp = techbase.sp['O3']
+        super(O3Core, self).__init__(mech=mech, tech=tech, pv=pv,
+                pen_adjust=pen_adjust, sigma_level=sigma_level)
 
 
 def plot_split(v, f, dp, sp, p, tech):
@@ -446,13 +420,14 @@ if __name__ == '__main__':
     """
     import matplotlib.pyplot as plt
     techList = (45, 32, 22, 16)
-    mechList = ('LP', 'HKMGS', 'ITRS', 'CONS')
+    #mechList = ('LP', 'HKMGS', 'ITRS', 'CONS')
+    mechList = ('HKMGS', )
     vsList = [x * 0.05 for x in xrange(6, 23)]
     for mech in mechList:
         fig = plt.figure()
         axes = fig.add_subplot(111)
         for tech in techList:
-            c = Core(tech=tech, mech=mech, ctype='IO')
+            c = IOCore(tech=tech, mech=mech)
             #c.config(tech=32)
             #print 'tech:%d, mech: %s, dp: %f, sp: %f, freq:%f' % (tech, mech, c.dp0, c.sp0, c.freq)
 
