896546813943e130fa2be370221aa631f5469cce
Liang Wang <lw2aw@virginia.edu>
2012-10-14 19:32:34 -0400

    add CLI interface for future use

--- main.py
+++ main.py
@@ -1,9 +1,48 @@
 #!/usr/bin/env python
 
-from analyses import final
-from analyses import ucore
+import os
+import atexit
+import rlcompleter
+import readline
+from cmd2 import Cmd
 
-if __name__ == '__main__':
-    #final.main()
-    ucore.main()
+class CLIApp(Cmd):
+    prompt = 'DimSi>'
+    intro = '''
+    DimSi analysis
+    '''
 
+    def __init__(self):
+        Cmd.__init__(self)
+        self._cwd = os.getcwd()
+
+    def default(self, arg):
+        self.do_shell(arg)
+
+    def complete_cd(self, text, line, begidx, endidx):
+        flist = [subdir for subdir in sorted(os.listdir(self._cwd))
+                 if os.path.isdir(os.path.join(self._cwd, subdir))]
+        if not text:
+            completions = flist
+        else:
+            completions = [ f for f in flist
+                           if f.startswith(text) ]
+        return completions
+
+    def do_cd(self, args):
+        ''' Change the current working directory
+        '''
+        os.chdir(args)
+        self._cwd = os.getcwd()
+
+historyPath = os.path.expanduser("./.pyhistory")
+if os.path.exists(historyPath):
+    readline.read_history_file(historyPath)
+
+def save_history(historyPath=historyPath):
+    readline.write_history_file(historyPath)
+
+atexit.register(save_history)
+
+app = CLIApp()
+app.cmdloop()
