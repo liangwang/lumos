31b25063b702c1c9d96bb5ac3ecd226a513a4d56
Liang Wang <lw2aw@virginia.edu>
2013-02-17 10:56:09 -0500

    document setting for readthedoc

--- doc/conf.py
+++ doc/conf.py
@@ -91,7 +91,11 @@ modindex_common_prefix = ['lumos.']
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'lumos'
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+if on_rtd:
+    html_theme = 'default'
+else:
+    html_theme = 'lumos'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
