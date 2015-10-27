a992e36a6201b4ab711feb9350ff596a0fd70af1
Liang Wang <lw2aw@virginia.edu>
2015-10-27 13:34:45 -0400

    add test cases for reconfig overhead

--- lumos/tests/test_appdetailed.py
+++ lumos/tests/test_appdetailed.py
@@ -1,26 +1,30 @@
 import os
 import lumos
 from lumos.model.workload import load_kernels_and_apps
-from lumos.model.system.hetero import HeterogSysDetailed as HetSys, SysConfigDetailed as HetSysConfig
-from lumos.model.system.homo import HomogSysDetailed as HomSys, SysConfigDetailed as HomSysConfig
+from lumos.model.system.hetero import HeterogSysDetailed as HetSys
+from lumos.model.system.hetero import SysConfigDetailed as HetSysConfig
+from lumos.model.system.homo import HomogSysDetailed as HomSys
+from lumos.model.system.homo import SysConfigDetailed as HomSysConfig
 from lumos.model.system.budget import Budget
 import unittest
+
 
 class TestDetailedApp(unittest.TestCase):
     def test_homog_vs_heterog_detailed(self):
         workload_xmlfile = os.path.join(
-            os.path.dirname(os.path.dirname(lumos.__file__)), 'workloads', 'sirius.xml')
+            os.path.dirname(lumos.model.workload.__file__), 'sirius.xml')
         _ks, _as = load_kernels_and_apps(workload_xmlfile)
         app = _as['synapp_0']
 
         hetsys = HetSys(HetSysConfig(), _ks)
         homsys = HomSys(HomSysConfig())
         self.assertAlmostEqual(hetsys.perf(650, app),
-                               homsys.perf(650, app), places=2)
+                               homsys.perf(650, app),
+                               places=2)
 
     def test_homog_vs_heterog_detailed2(self):
         workload_xmlfile = os.path.join(
-            os.path.dirname(os.path.dirname(lumos.__file__)), 'workloads', 'sirius.xml')
+            os.path.dirname(lumos.model.workload.__file__), 'sirius.xml')
         _ks, _as = load_kernels_and_apps(workload_xmlfile)
         app = _as['synapp_0']
 
@@ -29,7 +33,11 @@ class TestDetailedApp(unittest.TestCase):
         hetsys = HetSys(config, _ks)
 
         config2 = HomSysConfig()
-        config2.budget = Budget(power=config.budget.power, area=config.budget.area*0.8)
+        config2.budget = Budget(power=config.budget.power,
+                                area=config.budget.area * 0.8)
         homsys = HomSys(config2)
-        self.assertAlmostEqual(hetsys.perf(650, app, disable_rlacc=True),
-                               homsys.perf(650, app), places=2)
+        self.assertAlmostEqual(hetsys.perf(650,
+                                           app,
+                                           disable_rlacc=True),
+                               homsys.perf(650, app),
+                               places=2)
--- lumos/tests/test_heterogsys.py
+++ lumos/tests/test_heterogsys.py
@@ -7,15 +7,31 @@ from lumos.model.system.hetero import HeterogSysDetailed, SysConfigDetailed
 import unittest
 
 
-class TestHomogSys(unittest.TestCase):
+class TestHeterogSys(unittest.TestCase):
     def setUp(self):
         pass
 
     def test_heterogsys_detailed(self):
         workload_xmlfile = os.path.join(
-            os.path.dirname(os.path.dirname(lumos.__file__)), 'workloads', 'sirius.xml')
+            os.path.dirname(lumos.model.workload.__file__), 'sirius.xml')
         _ks, _as = load_kernels_and_apps(workload_xmlfile)
         app = _as['synapp_0']
         sysconfig = SysConfigDetailed()
         sys = HeterogSysDetailed(sysconfig, _ks)
         self.assertAlmostEqual(sys.perf(650, app), 92.113, places=2)
+
+    def test_reconfig_overhead(self):
+        workload_xmlfile = os.path.join(
+            os.path.dirname(lumos.model.workload.__file__), 'sirius.xml')
+        _ks, _as = load_kernels_and_apps(workload_xmlfile)
+        app = _as['synapp_reconfig_overhead_0']
+        sysconfig = SysConfigDetailed()
+        sysconfig.rlacc_area_ratio = 0.2
+        sys = HeterogSysDetailed(sysconfig, _ks)
+        self.assertAlmostEqual(sys.perf(750, app), 1083.076, places=2)
+        app = _as['synapp_reconfig_overhead_1']
+        self.assertAlmostEqual(sys.perf(750, app), 766.22, places=2)
+        app = _as['synapp_reconfig_overhead_2']
+        self.assertAlmostEqual(sys.perf(750, app), 686.615, places=2)
+        app = _as['synapp_reconfig_overhead_3']
+        self.assertAlmostEqual(sys.perf(750, app), 686.615, places=2)
